a2df4af125c7df3721142eb917974677818df0dd
Mathieu Lecarme <mlecarme@bearstech.com>
2015-01-12 17:43:09 +0100

    init and start.

--- wpfactory.py
+++ wpfactory.py
@@ -6,6 +6,8 @@ Wordpress factory.
 
 Usage:
     wpfactory scaffold
+    wpfactory init
+    wpfactory start
     wpfactory start mysql
     wpfactory start wordpress
     wpfactory build mysql
@@ -18,22 +20,77 @@ Options:
 __version__ = '0.1'
 
 from subprocess import Popen, PIPE
+import yaml
 
 
 def docker(*args):
+    print " ".join(['docker'] + list(args))
     p = Popen(['docker'] + list(args), stdout=PIPE, stderr=PIPE)
     p.wait()
     e = p.stderr.read()
     if e:
-        raise e
+        raise Exception(e)
     return p.stdout.read()
 
+def wp(*args):
+    args = ['exec', '-ti', 'wordpress', 'wp', '--allow-root'] + list(args)
+    return docker(*args)
+
+def mysql(*args):
+    args = ['exec', '-ti', 'wordpress', 'mysql', '-h', 'db', '--password=mypass', '-e'] + list(args)
+    return docker(*args)
 
 if __name__ == '__main__':
     from docopt import docopt
+    import os
+    import os.path
 
     arguments = docopt(__doc__, version='Wordpress Manager %s' % __version__)
-    print arguments
+
+    cwd = os.getcwd()
+
+    if arguments['scaffold']:
+        if not os.path.exists('wordpress'):
+            os.makedirs('wordpress')
+        if not os.path.exists('wordpress.yml'):
+            with open('wordpress.yml', 'w') as f:
+                f.write("""---
+
+# Scaffolded Wordpress Factory config file.
+
+
+url: test.example.lan:8000
+name: Wordpress Factory Test
+admin:
+    email: admin@example.lan
+    user: admin
+    password: password
+db:
+    name: test
+    user: test
+    pass: password
+""")
+
+    if arguments['init']:
+        with open('wordpress.yml', 'r') as f:
+            conf = yaml.load(f)
+        print mysql("CREATE DATABASE IF NOT EXISTS {name};".format(name=conf['db']['name']))
+        print mysql("CREATE USER '{user}'@'%' IDENTIFIED BY '{password}';".format(user=conf['db']['user'],
+                                                                               password=conf['db']['pass']))
+        print mysql("GRANT ALL ON {name}.* TO '{user}'@'%';".format(name=conf['db']['name'],
+                                                                     user=conf['db']['user']))
+        print mysql("FLUSH PRIVILEGES;")
+        print wp('core', 'download')
+        print wp('core', 'config', '--skip-check',
+                 '--dbname=%s' % conf['db']['name'],
+                 '--dbuser=%s' % conf['db']['user'],
+                 '--dbpass=%s' % conf['db']['pass'],
+                 '--dbhost=db'
+                 )
+        print wp('core', 'install', '--url=%s' % conf['url'],
+           '--title="%s"' % conf['name'], '--admin_email=%s' % conf['admin']['email'],
+           '--admin_user=%s' % conf['admin']['user'], '--admin_password=%s' %
+           conf['admin']['password'])
 
     if arguments['build']:
         if arguments['wordpress']:
@@ -41,4 +98,15 @@ if __name__ == '__main__':
         if arguments['mysql']:
             print docker('build', '-t', 'wordpress', './docker/wordpress')
 
+    if arguments['start']:
+        if arguments['wordpress']:
+            pid = docker('run',  '--name=wordpress', '-d', '-p', '8000:80',
+                         '--volume' , '%s/wordpress:/var/www/test/root' % cwd,
+                         '--link=mysql:db', 'wordpress')
+            with open('%s/wp.pid' % cwd, 'w') as f:
+                f.write(pid)
+        elif arguments['mysql']:
+            docker('run', '--name=mysql', '-d', '-p', '3306', 'mysql')
+        else:
+            pass
 
