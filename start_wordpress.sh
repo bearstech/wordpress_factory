504854fac21b366c3dee26381251382e202e232b
Mathieu Lecarme <mlecarme@bearstech.com>
2015-01-14 15:44:41 +0100

    sh are boring.

--- start_wordpress.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --name=wordpress -d -p 8000:80 --volume `pwd`/src/wordpress/:/var/www/test/root --link=mysql:db wordpress > wp.pid
--- upgrade_wordpress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker exec -ti `cat wp.pid` ./wp-cli.phar --allow-root --path=/var/www/test/root plugin update --all
-docker exec -ti `cat wp.pid` ./wp-cli.phar --allow-root --path=/var/www/test/root theme update --all
-docker exec -ti `cat wp.pid` ./wp-cli.phar --allow-root --path=/var/www/test/root core verify-checksums
-docker exec -ti `cat wp.pid` ./wp-cli.phar --allow-root --path=/var/www/test/root core update
-docker exec -ti `cat wp.pid` ./wp-cli.phar --allow-root --path=/var/www/test/root core update-db
-
-
