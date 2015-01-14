3e8eb0940ab1cddcc0b3f89a28030e0efba317c8
Stéphane Klein <contact@stephane-klein.info>
2015-01-14 15:15:01 +0100

    Coding style in setup.py + append entry points

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from setuptools import setup, find_packages
 
-setup(name='wpfactory',
-      version='0.1',
-      packages=find_packages('src'),
-      package_dir = {'':'src'},
-       package_data={
+setup(
+    name='wpfactory',
+    version='0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
         'wpfactory': ['docker/wordpress/*', 'docker/mysql/*'],
-       }
-      )
+    },
+    entry_points={
+        'console_scripts': ['wpfactory=wpfactory:main']
+    }
+)
