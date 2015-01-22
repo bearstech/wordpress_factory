0afd9399dd823244c2eb99d30667a3379cc34ec0
Johan Charpentier <jcharpentier@bearstech.com>
2015-01-22 22:44:52 +0100

    Get rid of some warnings

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='wpfactory',
+    description='A factory to prepare, deploy, upgrade your Wordpress.'+
+    " Also Docker.",
     version='0.1',
+    author='Mathieu Lecarme',
+    author_email='mlecarme@bearstech.com',
+    maintainer='Johan Charpentier',
+    maintainer_email='jcharpentier@bearstech.com',
+    url='https://github.com/bearstech/wordpress_factory',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     package_data={
