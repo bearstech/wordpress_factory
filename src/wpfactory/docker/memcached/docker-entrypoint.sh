f7ed9bcb5aeb2b906776995ca9423313968bd0f3
Mathieu Lecarme <mlecarme@bearstech.com>
2015-02-07 22:12:28 +0100

    Testing with Sasl auth.

--- src/wpfactory/docker/memcached/docker-entrypoint.sh
+++ src/wpfactory/docker/memcached/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-ARGUMENTS="memcached -l 0.0.0.0 -p 11211 -m 64 -u nobody -v"
+# FIXME -u memcache
+ARGUMENTS="memcached -l 0.0.0.0 -p 11211 -m 64 -u root -v"
 if [ "$MEMCACHED_USER" ]; then
+    echo "START=yes" >> /etc/default/saslauthd
+    /etc/init.d/saslauthd start
     echo $MEMCACHED_PASSWORD | saslpasswd2 -c -p -a memcached $MEMCACHED_USER
     ARGUMENTS="$ARGUMENTS -B binary -S"
+    echo "Adding user $MEMCACHED_USER"
 fi
 echo $ARGUMENTS > /start-memcached
 chmod +x /start-memcached
+echo $ARGUMENTS
 
 exec "$@"
