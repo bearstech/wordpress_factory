f640bfb6452151c2c5dc97a73f15b56a3fa054d6
Johan Charpentier <jcharpentier@bearstech.com>
2015-02-11 00:12:03 +0100

    Use entrypoints luke

---
+++ src/wpfactory/docker/wpcli/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'wp' ]; then
+	# read DATADIR from the MySQL config
+	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+	
+	if [ ! -f "$WP_CLI_CONFIG_PATH" ]; then
+        cat $WP_CLI_CONFIG_PATH <<-EOF
+		---
+
+		path: $WP_CLI_ROOT_PATH
+		require:
+			- /opt/dictator/dictator.php
+        EOF
+	fi
+    exec "$@"
+	
+fi
+
+exec "$@"
