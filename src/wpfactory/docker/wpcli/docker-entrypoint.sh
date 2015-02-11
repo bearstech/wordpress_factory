a312eab57c187b10d1a7ce5d5958aa2b9f709ea9
Johan Charpentier <jcharpentier@bearstech.com>
2015-02-11 10:02:23 +0100

    Mixing tabs and spaces is bad

--- src/wpfactory/docker/wpcli/docker-entrypoint.sh
+++ src/wpfactory/docker/wpcli/docker-entrypoint.sh
@@ -4,18 +4,17 @@ set -e
 if [ "$1" = 'wp' ]; then
 	# read DATADIR from the MySQL config
 	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
-	
-	if [ ! -f "$WP_CLI_CONFIG_PATH" ]; then
-        cat $WP_CLI_CONFIG_PATH <<-EOF
-		---
 
+	if [ ! -f "$WP_CLI_CONFIG_PATH" ]; then
+		cat $WP_CLI_CONFIG_PATH <<-EOF
+		---
 		path: $WP_CLI_ROOT_PATH
 		require:
 			- /opt/dictator/dictator.php
-        EOF
+		EOF
 	fi
-    exec "$@"
-	
+	exec "$@"
+
 fi
 
 exec "$@"
