079715e2765563586f16231cbcde2d4c28b794e3
Johan Charpentier <jcharpentier@bearstech.com>
2015-02-11 12:09:46 +0100

    Fix entrypoint and move wp-cli where user can modify it

--- src/wpfactory/docker/wpcli/docker-entrypoint.sh
+++ src/wpfactory/docker/wpcli/docker-entrypoint.sh
@@ -2,15 +2,12 @@
 set -e
 
 if [ "$1" = 'wp' ]; then
-	# read DATADIR from the MySQL config
-	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
-
 	if [ ! -f "$WP_CLI_CONFIG_PATH" ]; then
-		cat $WP_CLI_CONFIG_PATH <<-EOF
+		cat <<-EOF > $WP_CLI_CONFIG_PATH
 		---
 		path: $WP_CLI_ROOT_PATH
 		require:
-			- /opt/dictator/dictator.php
+		    - /opt/dictator/dictator.php
 		EOF
 	fi
 	exec "$@"
